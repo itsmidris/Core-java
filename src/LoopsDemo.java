2f3e0be7c49a73eb725706858d007ed4d20aa17d
itsmidris <idrishiimran72@gmail.com>
2026-06-04 14:21:15 +0530

    Added Java project structure

---
+++ src/LoopsDemo.java
@@ -0,0 +1,13 @@
+public class LoopsDemo {
+    public static void main() {
+        System.out.println("Hello World");
+
+        int[] arr= new int[5];
+        int []arr1={3,4,5,5,6,7,7};
+
+        //for each
+        for(int data:arr1){
+            System.out.println(data);
+        }
+    }
+}
